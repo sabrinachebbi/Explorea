db1704115acb881656c242bb497bedac1358791e
sabrine chebbi <sabrina.chebbi2@gmail.com>
2024-08-30 17:39:52 +0200

    progress/accommodation

--- src/Controller/AccommodationController.php
+++ src/Controller/AccommodationController.php
@@ -11,8 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use DateTimeImmutable;
-
-
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 
 #[Route('/accommodation', name: 'app_accommodation_')]
@@ -30,6 +29,7 @@ class AccommodationController extends AbstractController
 
     //fonction pour ajouter une accommodation
     #[Route('/new', name: 'new')]
+    #[IsGranted('ROLE_USER')]
 
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -51,13 +51,17 @@ class AccommodationController extends AbstractController
     }
 
     #[Route('/update/{id}', name: 'update')]
+    #[IsGranted('ROLE_USER')]
 
     public function update(
         Accommodation $accommodation,
         Request $request,
         EntityManagerInterface $entityManager)
       : Response
-    {
+    {   $user = $this->getUser();
+        if (!$user->getAccommodations()->contains($accommodation) && !$this->isGranted('ROLE_ADMIN')){
+              return $this->redirectToRoute('app_accommodation_showAll');
+        }
         $form = $this->createForm(AccommodationFormType::class, $accommodation);
         $form->handleRequest($request);
 
@@ -67,13 +71,14 @@ class AccommodationController extends AbstractController
             $entityManager->flush();
             return $this->redirectToRoute('app_accommodation_showAll');
         }
-        return $this->render('accommodation/newAccommodation.html.twig', [
+        return $this->render('accommodation/updateAccommodation.html.twig', [
             'AccommodationForm' => $form,
             'accommodation' => $accommodation,
         ]);
     }
     //fonction pour afficher les details d' une seul annonce
     #[Route('/show/{id}', name: 'showDetails')]
+    #[IsGranted('ROLE_USER')]
     public function show( Accommodation $accommodation): Response {
 
         return $this->render('accommodation/showAccommodation.html.twig',[
@@ -83,7 +88,12 @@ class AccommodationController extends AbstractController
 
     //fonction pour supprimer une annonce
     #[Route('/remove/{id}', name: 'remove')]
+    #[IsGranted('ROLE_USER')]
     public function remove(Request $request, Accommodation $accommodation, EntityManagerInterface $entityManager): Response {
+        $user = $this->getUser();
+        if (!$user->getAccommodations()->contains($accommodation) && !$this->isGranted('ROLE_ADMIN')){
+            return $this->redirectToRoute('app_accommodation_showAll');
+        }
         $token = $request->getPayload()->get('token');
 
         if($this->isCsrfTokenValid('delete-acommodation' . $accommodation->getId(), $token)){
